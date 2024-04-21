416e5daa70097f06dca303c03ad288448592ae0d
gracebir <grace.devolop@gmail.com>
2024-04-21 08:45:03 +0200

    search and filter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class=" md:px-6 lg:px-4 flex flex-col gap-6 lg:gap-8">
         <div class="flex flex-col lg:flex-row gap-8 justify-between">
-            <div class="lg:max-w-[500px] flex gap-2 w-full bg-bgDarlElt px-4 py-4 rounded-md">
+            <div class="lg:max-w-[500px] flex items-center gap-3 w-full bg-bgDarlElt px-4 py-4 rounded-md">
                 <SearchIcon style="height: 20px;"/>
-                <input class="w-full outline-none bg-bgDarlElt" type="text">
+                <input @input="handleSearch" placeholder="Search for a country..." class="w-full placeholder:text-gray-300 outline-none bg-bgDarlElt" type="text">
             </div>
             <div class="relative  w-[200px]">
               <div @click="isOpen = !isOpen" class="w-full bg-bgDarlElt py-4 px-6 min-w-36 flex justify-between items-center gap-2 cursor-pointer rounded-md">
@@ -29,8 +29,10 @@
 <script setup lang="ts">
 import CountryTile from '@/components/CountryTile.vue';
 import axiosClient from '@/axiosClient';
+
+import debounce from 'lodash/debounce'
 import {SearchIcon, ChevronDownIcon, ChevronLeftIcon} from '@heroicons/vue/outline'
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 
 type tCountries = {
   id: string
@@ -52,25 +54,51 @@ const countries = ref<tCountries[]>([])
 
 const region = ref<string>('Filter by Region')
 const isOpen = ref<boolean>(false)
+const searchText = ref<string>('');
+const url = ref<string>("/all")
+
+let url1 = "all"
 
 function handleOptionChange(country: string){
   region.value = country
   isOpen.value = false
 }
 
-// watch(region,()=> {
-
-// })
-
+const handleSearch = debounce((event: InputEvent) => {
+  const text = (event.target as HTMLInputElement).value;
+  searchText.value = text;
+}, 300);
 const regionOptions = [
   "Africa", "America", "Asia", "Europe","Oceania"
 ]
 
 async function getAllCountries(){
-      const response = await axiosClient.get('/all');
+
+      url.value = searchText.value.length > 3 ? `/name/${searchText.value}` : '/all';
+      if(searchText.value.length > 3){
+        region.value="Filter by Region"
+        url.value =  `/name/${searchText.value}`
+      } else {
+        url.value = '/all';
+      }
+
+      if(region.value !== "Filter by Region"){
+        searchText.value = ""
+        url.value = `/region/${region.value.toLocaleLowerCase()}`
+      }
+
+      const response = await axiosClient.get(url.value);
       const data: Array<tCountries> = await response.data
       countries.value = data ? data : []
 }
+
+watch(searchText, () => {
+  getAllCountries();
+});
+
+watch(region, () => {
+  getAllCountries();
+});
 
 onMounted(()=> {
   getAllCountries()
