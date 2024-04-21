d4fd5f4f67a5ac94cc12f3c7897c166c4ed46cc1
gracebir <grace.devolop@gmail.com>
2024-04-21 09:27:00 +0200

    adding some arrow

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -15,6 +15,11 @@ declare module "@heroicons/vue/outline" {
   export { default as SearchIcon } from "@heroicons/vue/outline/esm/SearchIcon.js"
   export { default as ChevronDownIcon } from "@heroicons/vue/outline/esm/ChevronDownIcon.js"
   export { default as ChevronLeftIcon } from "@heroicons/vue/outline/esm/ChevronLeftIcon.js"
+  export { default as ArrowDownIcon } from "@heroicons/vue/solid/esm/ArrowDownIcon.js"
+  export { default as ArrowLeftIcon } from "@heroicons/vue/solid/esm/ArrowLeftIcon.js"
+  export { default as ArrowNarrowDownIcon } from "@heroicons/vue/solid/esm/ArrowNarrowDownIcon.js"
+  export { default as ArrowNarrowLeftIcon } from "@heroicons/vue/solid/esm/ArrowNarrowLeftIcon.js"
+  export { default as ArrowNarrowRightIcon } from "@heroicons/vue/solid/esm/ArrowNarrowRightIcon.js"
   // rest of outline icons
 }
 
